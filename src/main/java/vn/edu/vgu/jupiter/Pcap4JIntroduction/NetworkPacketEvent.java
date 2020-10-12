7f5241ba386e56543592f701308fb31953f6322c
cyberstorage <thanh_an_cute@yahoo.com>
2020-10-13 00:36:23 +0700

    fix class NetworkPacketEvent

--- src/main/java/vn/edu/vgu/jupiter/Pcap4JIntroduction/NetworkPacketEvent.java
+++ src/main/java/vn/edu/vgu/jupiter/Pcap4JIntroduction/NetworkPacketEvent.java
@@ -1,17 +1,39 @@
 package vn.edu.vgu.jupiter.Pcap4JIntroduction;
 
+import org.pcap4j.packet.IpPacket;
+import org.pcap4j.packet.TcpPacket;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class NetworkPacketEvent {
+    private Timestamp timeStamp;
+    private Date date;
+
+    private TcpPacket.TcpHeader tcpHeader;
+    private IpPacket.IpHeader ipHeader;
+
     private String sourceAddress;
     private int sourcePort;
 
     private String destinationAddress;
     private int destinationPort;
 
-    public NetworkPacketEvent(String sourceAddress, int sourcePort, String destinationAddress, int destinationPort){
-        this.sourceAddress = sourceAddress;
-        this.sourcePort = sourcePort;
-        this.destinationAddress = destinationAddress;
-        this.destinationPort = destinationPort;
+    public NetworkPacketEvent(TcpPacket.TcpHeader tcpHeader, IpPacket.IpHeader ipHeader, Timestamp timeStamp){
+        this.tcpHeader = tcpHeader;
+        this.ipHeader = ipHeader;
+        this.timeStamp = timeStamp;
+
+        //PARSE DATA
+        sourceAddress = ipHeader.getSrcAddr().toString().substring(1);
+        destinationAddress = ipHeader.getDstAddr().toString().substring(1);
+
+        String[] tempArray = tcpHeader.getSrcPort().toString().split(" ");
+        sourcePort = Integer.parseInt(tempArray[0]);
+        tempArray = tcpHeader.getDstPort().toString().split(" ");
+        destinationPort = Integer.parseInt(tempArray[0]);
+
+        date = new Date(timeStamp.getTime());
     }
 
     public String getSourceAddress() { return sourceAddress; }
@@ -19,4 +41,6 @@ public class NetworkPacketEvent {
 
     public int getDestinationPort() { return destinationPort; }
     public int getSourcePort() { return sourcePort; }
+
+    public Date getDate() { return date; }
 }
