7281f72b56dc2679d2dbc755c25f1b59fa3413c9
letung3105 <15053897+letung3105@users.noreply.github.com>
2020-10-13 14:08:38 +0700

    add getters, remove date field

--- src/main/java/vn/edu/vgu/jupiter/Pcap4JIntroduction/NetworkPacketEvent.java
+++ src/main/java/vn/edu/vgu/jupiter/Pcap4JIntroduction/NetworkPacketEvent.java
@@ -4,43 +4,53 @@ import org.pcap4j.packet.IpPacket;
 import org.pcap4j.packet.TcpPacket;
 
 import java.sql.Timestamp;
-import java.util.Date;
 
 public class NetworkPacketEvent {
     private Timestamp timeStamp;
-    private Date date;
-
     private TcpPacket.TcpHeader tcpHeader;
     private IpPacket.IpHeader ipHeader;
-
     private String sourceAddress;
     private int sourcePort;
-
     private String destinationAddress;
     private int destinationPort;
 
-    public NetworkPacketEvent(TcpPacket.TcpHeader tcpHeader, IpPacket.IpHeader ipHeader, Timestamp timeStamp){
+    public NetworkPacketEvent(TcpPacket.TcpHeader tcpHeader, IpPacket.IpHeader ipHeader, Timestamp timeStamp) {
         this.tcpHeader = tcpHeader;
         this.ipHeader = ipHeader;
         this.timeStamp = timeStamp;
 
         //PARSE DATA
-        sourceAddress = ipHeader.getSrcAddr().toString().substring(1);
-        destinationAddress = ipHeader.getDstAddr().toString().substring(1);
-
-        String[] tempArray = tcpHeader.getSrcPort().toString().split(" ");
-        sourcePort = Integer.parseInt(tempArray[0]);
-        tempArray = tcpHeader.getDstPort().toString().split(" ");
-        destinationPort = Integer.parseInt(tempArray[0]);
-
-        date = new Date(timeStamp.getTime());
+        sourceAddress = ipHeader.getSrcAddr().getHostAddress();
+        sourcePort = tcpHeader.getSrcPort().valueAsInt();
+        destinationAddress = ipHeader.getDstAddr().getHostAddress();
+        destinationPort = tcpHeader.getDstPort().valueAsInt();
     }
 
-    public String getSourceAddress() { return sourceAddress; }
-    public String getDestinationAddress() { return destinationAddress; }
+    public Timestamp getTimeStamp() {
+        return timeStamp;
+    }
 
-    public int getDestinationPort() { return destinationPort; }
-    public int getSourcePort() { return sourcePort; }
+    public TcpPacket.TcpHeader getTcpHeader() {
+        return tcpHeader;
+    }
 
-    public Date getDate() { return date; }
+    public IpPacket.IpHeader getIpHeader() {
+        return ipHeader;
+    }
+
+    public String getSourceAddress() {
+        return sourceAddress;
+    }
+
+    public String getDestinationAddress() {
+        return destinationAddress;
+    }
+
+    public int getDestinationPort() {
+        return destinationPort;
+    }
+
+    public int getSourcePort() {
+        return sourcePort;
+    }
 }
