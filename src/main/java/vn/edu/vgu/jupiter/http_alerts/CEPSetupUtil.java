991ac71ed40853c7b10818ab0e6a0edf9e1825e2
Bui Xuan Phuoc <47053032+unneros@users.noreply.github.com>
2020-10-20 22:48:57 +0700

    added package declaration

--- src/main/java/vn/edu/vgu/jupiter/http_alerts/CEPSetupUtil.java
+++ src/main/java/vn/edu/vgu/jupiter/http_alerts/CEPSetupUtil.java
@@ -1,3 +1,5 @@
+package vn.edu.vgu.jupiter.http_alerts;
+
 import com.espertech.esper.common.client.EPCompiled;
 import com.espertech.esper.common.client.configuration.Configuration;
 import com.espertech.esper.compiler.client.CompilerArguments;
