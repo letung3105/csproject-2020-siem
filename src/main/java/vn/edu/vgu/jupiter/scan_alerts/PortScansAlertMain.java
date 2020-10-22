e372ced43bfaeafe21eeab851a779ed277f9539e
cyberstorage <thanh_an_cute@yahoo.com>
2020-10-22 08:27:38 +0700

    add fxml fields for dashboard, add function for dashboard, modify PortscanMain

--- src/main/java/vn/edu/vgu/jupiter/scan_alerts/PortScansAlertMain.java
+++ src/main/java/vn/edu/vgu/jupiter/scan_alerts/PortScansAlertMain.java
@@ -18,7 +18,7 @@ public class PortScansAlertMain implements Runnable{
      * The predefined variables by program
      */
     private static final Logger log = LoggerFactory.getLogger(PortScansAlertMain.class);
-    private static final int COUNT = -1;
+    private static int COUNT = -1;
     private static final int READ_TIMEOUT = 100; // [ms]
     private static final int SNAPLEN = 65536; // [bytes]
     private static final String FILTER = "tcp";
@@ -31,61 +31,85 @@ public class PortScansAlertMain implements Runnable{
     private int minConnectionCountVertical = 60; // [connections] [info threshold]
     private int timeWindowVertical = 60; // [seconds]
     private int countVertical = 100; // [warning threshold]
+    private int intervalVertical = 10; // [seconds delay]
 
     private int minConnectionCountHorizontal = 60; // [connections] [info threshold]
     private int timeWindowHorizontal = 60; // [seconds]
     private int countHorizontal = 100; // [warning threshold]
+    private int intervalHorizontal = 10; // [seconds delay]
 
     private int minPortsCount = 50; // [ports]
     private int minAddressCount = 2; // [addresses] [info threshold]
     private int timeWindowBlock = 60; // [seconds]
     private int countBlock = 5; // [warning threshold]
+    private int intervalBlock = 10; // [seconds delay]
 
     public PortScansAlertMain(String netDevName){
         this.netDevName = netDevName;
     }
 
+    //Some condition variable
+    private boolean isVariableChange = true;
+
     /**
      * Setup the runtime, deploys the necessary statements and starts capturing packets
      */
     public void run() {
-        try {
-            Configuration configuration = PortScansAlertUtil.getConfiguration();
-            EPRuntime runtime = EPRuntimeProvider.getRuntime(this.getClass().getSimpleName(), configuration);
+        Configuration configuration = PortScansAlertUtil.getConfiguration();
+        EPRuntime runtime = EPRuntimeProvider.getRuntime(this.getClass().getSimpleName(), configuration);
 
-            // TODO: statement's parameters should be modifiable from external classes
-            // compile and deploy epl statements
-            log.info("Setting up EPL");
-            new TcpPacketWithClosedPortStatement(runtime);
-            new VerticalPortScanAlertStatement(runtime, minConnectionCountVertical, timeWindowVertical, 10, countVertical);
-            new HorizontalPortScanAlertStatement(runtime, minConnectionCountHorizontal, timeWindowHorizontal, 10, countHorizontal);
-            new BlockPortScanAlertStatement(runtime, minPortsCount, minAddressCount, timeWindowBlock, 10, countBlock);
+        while(true){
+            if(isVariableChange){
+                isVariableChange = false;
+                COUNT = -1;
+                //rerun the thing
+                try{
+                    // TODO: statement's parameters should be modifiable from external classes
+                    // compile and deploy epl statements
+                    log.info("Setting up EPL");
+                    new TcpPacketWithClosedPortStatement(runtime);
+                    new VerticalPortScanAlertStatement(runtime, minConnectionCountVertical, timeWindowVertical, intervalVertical, countVertical);
+                    new HorizontalPortScanAlertStatement(runtime, minConnectionCountHorizontal, timeWindowHorizontal, intervalHorizontal, countHorizontal);
+                    new BlockPortScanAlertStatement(runtime, minPortsCount, minAddressCount, timeWindowBlock, intervalBlock, countBlock);
 
-            // getting the network interface
-            PcapNetworkInterface nif = Pcaps.getDevByName(netDevName);
-            log.info(nif.getName() + "(" + nif.getDescription() + ")");
+                    // getting the network interface
+                    PcapNetworkInterface nif = Pcaps.getDevByName(netDevName);
+                    log.info(nif.getName() + "(" + nif.getDescription() + ")");
 
-            final PcapHandle handle = nif.openLive(SNAPLEN, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, READ_TIMEOUT);
-            handle.setFilter(FILTER, BpfProgram.BpfCompileMode.OPTIMIZE);
-            try {
-                // capturing packet and send the Esper engine
-                handle.loop(COUNT, (PacketListener) packet -> {
-                    IpV4Packet ipV4Packet = packet.get(IpV4Packet.class);
-                    TcpPacket tcpPacket = ipV4Packet.get(TcpPacket.class);
-                    TcpPacketEvent evt = new TcpPacketEvent(
-                            handle.getTimestamp().getTime(),
-                            tcpPacket.getHeader(),
-                            ipV4Packet.getHeader()
-                    );
-                    runtime.getEventService().sendEventBean(evt, TcpPacketEvent.class.getSimpleName());
-                });
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                    final PcapHandle handle = nif.openLive(SNAPLEN, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, READ_TIMEOUT);
+                    handle.setFilter(FILTER, BpfProgram.BpfCompileMode.OPTIMIZE);
+                    try {
+                        // capturing packet and send the Esper engine
+                        handle.loop(COUNT, (PacketListener) packet -> {
+                            IpV4Packet ipV4Packet = packet.get(IpV4Packet.class);
+                            TcpPacket tcpPacket = ipV4Packet.get(TcpPacket.class);
+                            TcpPacketEvent evt = new TcpPacketEvent(
+                                    handle.getTimestamp().getTime(),
+                                    tcpPacket.getHeader(),
+                                    ipV4Packet.getHeader()
+                            );
+                            runtime.getEventService().sendEventBean(evt, TcpPacketEvent.class.getSimpleName());
+                            if(isVariableChange){
+                                COUNT = 1;
+                                log.info("Variable change detect, rerun the loop.");
+                            }
+                        });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    handle.close();
+                    //undeploy
+                    runtime.getDeploymentService().undeployAll();
+                    log.info("Undeploy all.");
+                }catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
             }
-            handle.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
+    }
+
+    public void setVariableChange(boolean variableChange) {
+        isVariableChange = variableChange;
     }
 
     /**
@@ -169,5 +193,17 @@ public class PortScansAlertMain implements Runnable{
 
     public void setCountBlock(int countBlock) {
         this.countBlock = countBlock;
+    }
+
+    public void setIntervalVertical(int intervalVertical) {
+        this.intervalVertical = intervalVertical;
+    }
+
+    public void setIntervalHorizontal(int intervalHorizontal) {
+        this.intervalHorizontal = intervalHorizontal;
+    }
+
+    public void setIntervalBlock(int intervalBlock) {
+        this.intervalBlock = intervalBlock;
     }
 }
